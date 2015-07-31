c3dc8fd1c58162e80edcc258589b22e17bcf552f
Michael Gautier <michaelqgautier@gmail.com>
2015-07-31 11:08:02 -0500

    appended comment

--- src/gautier_visual_model.cxx
+++ src/gautier_visual_model.cxx
@@ -190,4 +190,5 @@ ns_visual_model::screen_build::print_defined_screens(list_type_ptr_screen screen
 
 	return;
 }
+//Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 . Software distributed under the License is distributed on an "AS IS" BASIS, NO WARRANTIES OR CONDITIONS OF ANY KIND, explicit or implicit. See the License for details on permissions and limitations.
 
